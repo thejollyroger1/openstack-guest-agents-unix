17aa2e38c2c2413cc28e481ef579a82a2ea6bd8c
shivaling sannalli <shivaling.sannalli@RACKSPACE.COM>
2013-08-13 14:53:41 +0530

    agent installer changes

--- tools/install_agent.py
+++ tools/install_agent.py
@@ -13,6 +13,7 @@ def install_tar():
     :rtype : object
  """
     agent_tar_path = "/root/nova-agent/nova-agent-Linux-x86_64-0.0.1.37.tar.gz"
+    nova_file = "nova-agent-Linux-x86_64-0.0.1.37.tar.gz"
     installer_path = "/root/nova-agent/"
     nova_agent__process_path = "/etc/init.d/nova-agent"
     nova_agent_path = "/usr/share/nova-agent/"
@@ -22,14 +23,17 @@ def install_tar():
     if os.path.exists(nova_agent_path):
         shutil.move(nova_agent_path, "/tmp")
 
-    #subprocess.call(["mv", "%s" % nova_agent_path, "/tmp"])
-    subprocess.call(["tar", "-zvxf", "%s" % agent_tar_path])
-    time.sleep(5)
+    if not os.path.exists(installer_path):
+        os.mkdir(installer_path)
+
+    shutil.move(nova_file, installer_path)
     os.chdir(installer_path)
-    #subprocess.call(["cd", "%s" % installer_path])
+    subprocess.call(["tar", "-zvxf", "%s" % agent_tar_path])
+    time.sleep(8)
     subprocess.call(["sh", "installer.sh"])
-    time.sleep(2)
+    time.sleep(5)
     subprocess.call(["%s" % nova_agent__process_path, "start"])
+    subprocess.call(["rm", "-rf", "%s" % installer_path])
 
 
 install_tar()
