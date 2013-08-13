b109ae330c3a3053749b445a7a304d72553ec59e
shivaling sannalli <shivaling.sannalli@RACKSPACE.COM>
2013-08-13 18:49:12 +0530

    agent tester changes

--- tools/install_agent.py
+++ tools/install_agent.py
@@ -4,11 +4,38 @@ import os
 import subprocess
 import shutil
 import time
+import platform
+
+
+def install_uuid():
+    """
+
+    :rtype : object
+    """
+    os.chdir("/root")
+    subprocess.call(["curl", "-Lko", "uuid-1.30.tar.gz",
+                     "https://pypi.python.org/packages/source/u/uuid/"
+                     "uuid-1.30.tar.gz#md5=639b310f1fe6800e4bf8aa1dd9333117"])
+    os.mkdir("/root/uuid")
+    shutil.move("uuid-1.30.tar.gz", "/root/uuid")
+    os.chdir("/root/uuid")
+    subprocess.call(["tar", "-zvxf", "uuid-1.30.tar.gz"])
+    time.sleep(4)
+    os.chdir("/root/uuid/uuid-1.30/")
+    subprocess.call(["python", "setup.py", "install"])
+    os.chdir("/root")
+
+
+def identify_distro():
+    """
+    :return:
+    """
+    distro = platform.dist()[0]
+    return distro
 
 
 def install_tar():
     """
-
 
     :rtype : object
  """
@@ -35,6 +62,11 @@ def install_tar():
     time.sleep(5)
     subprocess.call(["%s" % nova_agent__process_path, "start"])
     subprocess.call(["rm", "-rf", "%s" % installer_path])
+
+    if 'redhat' in platform.dist():
+        if int(float(platform.dist()[1])) == 5:
+        #if (set(['redhat', '5.6']).issubset(set(platform.dist()))):
+            install_uuid()
 
 
 install_tar()
