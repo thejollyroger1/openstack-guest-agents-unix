5710bc0169cb78d76b379e4a8285fda01a97028c
AbhishekKr <abhikumar163@gmail.com>
2013-08-16 17:15:14 +0530

    updated debian network to use resolvconf util if present else create resolv.conf

---
+++ commands/utils.py
@@ -0,0 +1,34 @@
+# vim: tabstop=4 shiftwidth=4 softtabstop=4
+#
+#  Copyright (c) 2011 Openstack, LLC.
+#  All Rights Reserved.
+#
+#     Licensed under the Apache License, Version 2.0 (the "License"); you may
+#     not use this file except in compliance with the License. You may obtain
+#     a copy of the License at
+#
+#          http://www.apache.org/licenses/LICENSE-2.0
+#
+#     Unless required by applicable law or agreed to in writing, software
+#     distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#     WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#     License for the specific language governing permissions and limitations
+#     under the License.
+#
+
+"""
+utils module with common utility set commands
+"""
+
+# Utilities:
+# - is_system_command(<command_name>) : boolean for command in system exec path
+
+import sys
+import subprocess
+
+def is_system_command(cmd="python"):
+    return subprocess.call("type " + cmd,
+        shell=True,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE
+        ) == 0
