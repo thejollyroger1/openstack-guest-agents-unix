1ac428e97530a1137ad85c84fb25cf8148ce417a
shivaling sannalli <shivaling.sannalli@RACKSPACE.COM>
2013-08-29 15:12:19 +0530

    test runner changes

--- tools/agent_test_runner.py
+++ tools/agent_test_runner.py
@@ -7,47 +7,53 @@ import fabric
 from fabric.api import env, run
 from fabric.operations import put
 
+branch = "tests"
+
+def minion():
+    print(">>>>>>> %s" % run('uname -s'))
+
 
 def install_python_and_curl():
-    #Redhat Releases
-    run("if [ -f /etc/redhat-release ]; "
-        "then IF_PYTHON=`which python > /dev/null ; echo $?` ; if [[ $IF_PYTHON == '1' ]]; "
-        "then apt-get -y install python; fi;"
-        "IF_PYTHON=`which curl > /dev/null ; echo $?` ; if [[ $IF_PYTHON == '1' ]]; "
-        "then apt-get -y install curl; fi fi")
+    try:
+        #Redhat Releases
+        run("if [ -f /etc/redhat-release ]; "
+            "then IF_PYTHON=`which python > /dev/null ; echo $?` ; if [[ $IF_PYTHON == '1' ]]; "
+            "then apt-get -y install python; fi;"
+            "IF_PYTHON=`which curl > /dev/null ; echo $?` ; if [[ $IF_PYTHON == '1' ]]; "
+            "then apt-get -y install curl; fi fi")
 
-    #Open-SuSE image
-    run("if [ -f /etc/SuSE-release ]; "
-        "then IF_PYTHON=`which python > /dev/null ; echo $?` ; if [[ $IF_PYTHON == '1' ]]; "
-        "then zypper install -y python; fi;"
-        "IF_PYTHON=`which curl > /dev/null ; echo $?` ; if [[ $IF_PYTHON == '1' ]]; "
-        "then zypper install -y curl; fi fi")
+        #Open-SuSE image
+        run("if [ -f /etc/SuSE-release ]; "
+            "then IF_PYTHON=`which python > /dev/null ; echo $?` ; if [[ $IF_PYTHON == '1' ]]; "
+            "then zypper install -y python; fi;"
+            "IF_PYTHON=`which curl > /dev/null ; echo $?` ; if [[ $IF_PYTHON == '1' ]]; "
+            "then zypper install -y curl; fi fi")
 
-    #Debian images
-    run("if [ -f /etc/debian_version ]; "
-        "then IF_PYTHON=`which python > /dev/null ; echo $?` ; if [[ $IF_PYTHON == '1' ]]; "
-        "then apt-get install -y python; fi;"
-        "IF_PYTHON=`which curl > /dev/null ; echo $?` ; if [[ $IF_PYTHON == '1' ]]; "
-        "then apt-get install -y curl; fi fi")
+        #Debian images
+        run("if [ -f /etc/debian_version ]; "
+            "then IF_PYTHON=`which python > /dev/null ; echo $?` ; if [[ $IF_PYTHON == '1' ]]; "
+            "then apt-get install -y python; fi;"
+            "IF_PYTHON=`which curl > /dev/null ; echo $?` ; if [[ $IF_PYTHON == '1' ]]; "
+            "then apt-get install -y curl; fi fi")
 
-    #FreeBSD images
-    run("if [ `uname -s` == 'FreeBSD' ]; "
-        "then IF_PYTHON=`which python > /dev/null ; echo $?` ; if [[ $IF_PYTHON == '1' ]]; "
-        "then pkg_add -r python; fi;"
-        "then IF_BASH=`which bash > /dev/null ; echo $?` ; if [[ $IF_BASH == '1' ]]; "
-        "then pkg_add -r bash; fi;"
-        "IF_PYTHON=`which curl > /dev/null ; echo $?` ; if [[ $IF_PYTHON == '1' ]]; "
-        "then pkg_add -r curl; fi fi")
+    except:
+        #FreeBSD images fail for no env.shell bash
+        env.shell = '/bin/csh -c'
+        run("pkg_add -fr curl")
+        run("pkg_add -fr python")
+        run("pkg_add -fr bash")
+        run("ln -sf /usr/local/bin/bash /bin/bash")
+        env.shell = '/bin/bash -l -c'
 
 
 def install_agent():
-    run("curl -OkL https://raw.github.com/rackerlabs/openstack-guest-agents-unix/master/tests/functional/install_agent.py")
+    run("curl -OkL https://raw.github.com/rackerlabs/openstack-guest-agents-unix/%s/tests/functional/install_agent.py" % branch)
     run("python install_agent.py")
     run("rm -rf install_agent.py")
 
 
 def run_tests():
-    run("curl -OkL https://raw.github.com/rackerlabs/openstack-guest-agents-unix/master/tests/functional/agent_tester.py")
+    run("curl -OkL https://raw.github.com/rackerlabs/openstack-guest-agents-unix/%s/tests/functional/agent_tester.py" % branch)
     run("python agent_tester.py")
     run("rm -rf agent_tester.py")
 
@@ -60,6 +66,14 @@ def install_agent_and_run_tests():
     fabric.operations.reboot(120)
     run_tests()
 
+def create_agent_tar():
+    install_python_and_curl()
+    run("curl -Lk https://raw.github.com/rackerlabs/openstack-guest-agents-unix/%s/tools/nova-agent-builder.sh | bash" % branch)
+
+
+def install_agent_from_local_tar():
+    run("curl -OkL https://raw.github.com/rackerlabs/openstack-guest-agents-unix/%s/tests/functional/install_agent.py" % branch)
+    run("python install_agent.py --local /root/nova-agent/artifacts/nova-agent-FreeBSD-amd64-0.0.1.38.tar.gz")
 
 def load_config():
     config = RawConfigParser()
