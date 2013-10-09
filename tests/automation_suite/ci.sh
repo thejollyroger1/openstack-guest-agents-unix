2b6cf33e9dc11a0bae7bac79f3e74f5001776269
AbhishekKr <abhikumar163@gmail.com>
2013-10-09 16:26:52 +0530

    ci: ci.sh enhanced to adapt to better downloader or error if reqd

--- tests/automation_suite/ci.sh
+++ tests/automation_suite/ci.sh
@@ -52,21 +52,29 @@ cp -f $NOVA_AGENT_CFG "${NOVA_AGENT_TEST_BASEDIR}/tools/server_configurations.cf
 
 RELEASE_TAG=`curl -skL https://api.github.com/repos/rackerlabs/openstack-guest-agents-unix/tags | grep '"name":' | head -1 | awk -F'"' '{print $4}' | sed 's/^v//'`
 
-cd $NOVA_AGENT_LOCAL_BASE
-if [ $NOVA_AGENT_DISTRO == 'bsd' ]; then
-  curl -C - -sLkO "https://github.com/rackerlabs/openstack-guest-agents-unix/releases/download/v${RELEASE_TAG}/nova-agent-FreeBSD-amd64-${RELEASE_TAG}.tar.gz"
+if [ `which wget > /dev/null ; echo $?` == "0" ]; then
+  export NOVA_AGENT_DOWNLOADER="wget -c"
+elif [ `which curl > /dev/null ; echo $?` == "0" ]; then
+  export NOVA_AGENT_DOWNLOADER="curl -skLO"
 else
-  curl -C - -sLkO "https://github.com/rackerlabs/openstack-guest-agents-unix/releases/download/v${RELEASE_TAG}/nova-agent-Linux-x86_64-${RELEASE_TAG}.tar.gz"
+  echo "ERROR: Curl/Wget not found." ; exit 127
 fi
-curl -C - -sLkO "https://raw.github.com/rackerlabs/openstack-guest-agents-unix/${NOVA_AGENT_BRANCH}/tests/automation_suite/tools/install_prerequisite.sh"
-curl -C - -sLkO "https://raw.github.com/rackerlabs/openstack-guest-agents-unix/${NOVA_AGENT_BRANCH}/tests/automation_suite/tools/install_agent.py"
-curl -C - -sLkO "https://raw.github.com/rackerlabs/openstack-guest-agents-unix/${NOVA_AGENT_BRANCH}/tests/automation_suite/agent_tester.py"
-cd $NOVA_AGENT_TEST_BASEDIR
 
-echo "https://raw.github.com/rackerlabs/openstack-guest-agents-unix/${NOVA_AGENT_BRANCH}/tests/automation_suite/agent_test_runner.py"
-curl -C - -sLk -o "${NOVA_AGENT_TEST_BASEDIR}/agent_test_runner.py" "https://raw.github.com/rackerlabs/openstack-guest-agents-unix/${NOVA_AGENT_BRANCH}/tests/automation_suite/agent_test_runner.py"
-touch "${NOVA_AGENT_TEST_BASEDIR}/tools/__init__.py"
-curl -C - -sLk -o "${NOVA_AGENT_TEST_BASEDIR}/tools/server_creator.py" "https://raw.github.com/rackerlabs/openstack-guest-agents-unix/${NOVA_AGENT_BRANCH}/tests/automation_suite/tools/server_creator.py"
+cd $NOVA_AGENT_LOCAL_BASE
+if [ $NOVA_AGENT_DISTRO == 'FreeBSD' ]; then
+  $NOVA_AGENT_DOWNLOADER "https://github.com/rackerlabs/openstack-guest-agents-unix/releases/download/v${RELEASE_TAG}/nova-agent-FreeBSD-amd64-${RELEASE_TAG}.tar.gz"
+else
+  $NOVA_AGENT_DOWNLOADER "https://github.com/rackerlabs/openstack-guest-agents-unix/releases/download/v${RELEASE_TAG}/nova-agent-Linux-x86_64-${RELEASE_TAG}.tar.gz"
+fi
+$NOVA_AGENT_DOWNLOADER "https://raw.github.com/rackerlabs/openstack-guest-agents-unix/${NOVA_AGENT_BRANCH}/tests/automation_suite/tools/install_prerequisite.sh"
+$NOVA_AGENT_DOWNLOADER "https://raw.github.com/rackerlabs/openstack-guest-agents-unix/${NOVA_AGENT_BRANCH}/tests/automation_suite/tools/install_agent.py"
+$NOVA_AGENT_DOWNLOADER "https://raw.github.com/rackerlabs/openstack-guest-agents-unix/${NOVA_AGENT_BRANCH}/tests/automation_suite/agent_tester.py"
+
+cd $NOVA_AGENT_TEST_BASEDIR
+$NOVA_AGENT_DOWNLOADER "https://raw.github.com/rackerlabs/openstack-guest-agents-unix/${NOVA_AGENT_BRANCH}/tests/automation_suite/agent_test_runner.py"
+cd "${NOVA_AGENT_TEST_BASEDIR}/tools"
+$NOVA_AGENT_DOWNLOADER "https://raw.github.com/rackerlabs/openstack-guest-agents-unix/${NOVA_AGENT_BRANCH}/tests/automation_suite/tools/__init__.py"
+$NOVA_AGENT_DOWNLOADER "https://raw.github.com/rackerlabs/openstack-guest-agents-unix/${NOVA_AGENT_BRANCH}/tests/automation_suite/tools/server_creator.py"
 
 IF_FAB=`which fab > /dev/null ; echo $?`
 IF_PIP=`which pip > /dev/null ; echo $?`
