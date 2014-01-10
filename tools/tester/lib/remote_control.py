88b36232389b779a5e7ad84c31f29a413a36b057
AbhishekKr <abhikumar163@gmail.com>
2014-01-10 20:25:02 +0530

    [tester] remote-control got test_nova_agent; need to fix install_package

--- tools/tester/lib/remote_control.py
+++ tools/tester/lib/remote_control.py
@@ -4,6 +4,7 @@ from fabric.api import local, run, put, get, cd
 from fabric.context_managers import env, settings
 from fabric.tasks import execute
 import fabric.network
+import os
 
 
 def uptime(host, user, password, shell="bash -l -c"):
@@ -26,7 +27,6 @@ def create_nova_agent_bintar(host, user, password, shell="bash -l -c"):
                   use_ssh_config=False,
                   warn_only=True,
                   abort_on_prompts=True):
-        import os
         this_dir = os.path.dirname(os.path.realpath(__file__)) + "/.."
         fyl_path = "%s/../%s" % (this_dir, 'nova-agent-builder.sh')
         run("rm -rf /tmp/test_nova_agent/nova-agent; mkdir -p /tmp/test_nova_agent/nova-agent")
@@ -46,7 +46,6 @@ def update_nova_agent(host, user, password, shell="bash -l -c"):
                   use_ssh_config=False,
                   warn_only=True,
                   abort_on_prompts=True):
-        import os
         fyl_name = "nova-agent-Linux-x86_64-1.39.0.tar.gz"
         this_dir = os.path.dirname(os.path.realpath(__file__)) + "/.."
         this_dir = "%s/../../.temp/artifacts" % this_dir
@@ -67,3 +66,38 @@ def update_nova_agent(host, user, password, shell="bash -l -c"):
             elif run("ls /etc/systemctl"):
                 run("systemctl nova-agent restart")
     fabric.network.disconnect_all()
+
+
+def test_nova_agent(host, user, password, shell="bash -l -c"):
+    with settings(host_string=host,
+                  user=user,
+                  password=password,
+                  shell=shell,
+                  use_ssh_config=False,
+                  warn_only=True,
+                  abort_on_prompts=True):
+        #install_package("python")
+
+        fyl_name = "agent_tester.py"
+        this_dir = os.path.dirname(os.path.realpath(__file__)) + "/../../../tests/automation_suite"
+        this_fyl = "%s/%s" % (this_dir, fyl_name)
+        that_dir = "/tmp"
+        that_fyl = "%s/%s" % (that_dir, fyl_name)
+        put(this_fyl, that_fyl)
+        run("chmod +x %s" % that_fyl)
+        run(that_fyl)
+
+
+def install_package(pkg_name):
+    if run("ls /etc/redhat-release"):
+        run("yum install -y %s" % pkg_name)
+    elif run("ls /etc/debian_version"):
+        run("apt-get -y install %s" % pkg_name)
+    elif run("ls /etc/gentoo-release"):
+        run("emerge %s" % pkg_name)
+    elif run("ls /etc/arch-release"):
+        run("pacman -Sy --noconfirm %s" % pkg_name)
+    elif run("ls /etc/SuSE-release"):
+        run("zypper install -y %s" % pkg_name)
+    elif run("ls /etc/*bsd*"):
+        run("pkg_add -r %s" % pkg_name)
