d5bd3eb0081cd90446538c1d3b1a77b6c2d56b32
Nate House <nathan.house@rackspace.com>
2014-05-23 16:30:32 -0500

    Added delay on network reset

--- commands/suse/network.py
+++ commands/suse/network.py
@@ -79,6 +79,7 @@ def configure_network(hostname, interfaces):
         return (500, "Couldn't set hostname: %s" % str(e))
 
     # Restart network
+    time.sleep(2)
     logging.debug('executing systemctl restart network.service')
     p = subprocess.Popen(["systemctl", "restart", "network.service"],
             stdin=pipe, stdout=pipe, stderr=pipe, env={})
